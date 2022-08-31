fd2665cfa1382aa02ea42a2aadde7c7d99ffaf04
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2022-09-01 02:07:37 +0700

    work3php

---
+++ post.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 align="center">Webboard ZUZPAIZXsep</h1>
+    <hr>
+    <br>
+    <div align="center">
+   
+    <h2>ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?><br></h2>
+    <table style="border: 2px solid black; width: 40%" align="center">
+        <tr><td style="background-color: #6CD2FE;" colspan="2">แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea name="comment" id="" cols="50" rows="10"></textarea></td></tr>
+        <tr><td><input type="submit" value="ส่งข้อความ"/></td></tr>
+            
+                
+           
+        </table> 
+
+    </div><br>
+    <div align ="center"><a href="index.html">กลับไปหน้าหลัก</a></div>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 align="center">Webboard ZUZPAIZXsep</h1>
+    <hr>
+    <br>
+    <div align="center">
+    <b>เข้าสู่ระบบด้วย</b><br>
+    <?php echo "Login = ". $_POST["login"] ?><br>
+    <?php echo "Password = ". $_POST["pwd"]?>
+    </div>
+</body>
+</html>
